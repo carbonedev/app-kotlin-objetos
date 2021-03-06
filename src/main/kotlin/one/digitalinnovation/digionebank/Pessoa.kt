a56be197af61f8d4d52506b99207efa5b710584b
Denis Carbone <arboness@gmail.com>
2021-03-06 09:10:25 -0300

    new commit

--- src/main/kotlin/one/digitalinnovation/digionebank/Pessoa.kt
+++ src/main/kotlin/one/digitalinnovation/digionebank/Pessoa.kt
@@ -1,4 +1,12 @@
 package one.digitalinnovation.digionebank
 
 class Pessoa {
+    var nome: String = "Denis"
+    var cpf: String = "123.456.789-00"
+}
+fun main() {
+    val denis = Pessoa()
+
+    println(denis.nome)
+    println(denis.cpf)
 }
