3d05fe0ccdb6feb20c2bd7a54a9695e9c018fdab
Denis Carbone <arboness@gmail.com>
2021-03-06 17:10:22 -0300

    created Data Class

--- src/main/kotlin/one/digitalinnovation/digionebank/conta.kt
+++ src/main/kotlin/one/digitalinnovation/digionebank/conta.kt
@@ -2,11 +2,11 @@ package one.digitalinnovation.digionebank
 
 import java.math.BigDecimal
 
-class conta {
-    val agencia: String
-    val numero: String
+class conta (
+    val agencia: String,
+    val numero: String,
     val saldo: BigDecimal
-} {
+) {
     fun deposito(valor: BigDecimal) {
 
     }
