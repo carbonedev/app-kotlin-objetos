a79042c93f7a6829dcde7dcf754ebb7925c4eb4e
Denis Carbone <arboness@gmail.com>
2021-03-06 11:24:19 -0300

    Revert "created member"
    This reverts commit ee4362ee

--- src/main/kotlin/one/digitalinnovation/digionebank/Pessoa.kt
+++ src/main/kotlin/one/digitalinnovation/digionebank/Pessoa.kt
@@ -1,4 +1,17 @@
 package one.digitalinnovation.digionebank
 
 class Pessoa {
+    var nome: String = "Denis"
+    var cpf: String = "123.456.789-00"
+    private set
+
+    constructor()
+
+    fun pessoaInfo() = "$nome e $cpf"
+
+}
+
+fun main() {
+    val denis = Pessoa()
+    println(denis.pessoaInfo())
 }
