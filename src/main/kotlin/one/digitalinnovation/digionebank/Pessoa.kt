ee4362eee4217e9db767980ff633d48b75c6a01d
Denis Carbone <arboness@gmail.com>
2021-03-06 11:01:47 -0300

    created member

--- src/main/kotlin/one/digitalinnovation/digionebank/Pessoa.kt
+++ src/main/kotlin/one/digitalinnovation/digionebank/Pessoa.kt
@@ -3,10 +3,15 @@ package one.digitalinnovation.digionebank
 class Pessoa {
     var nome: String = "Denis"
     var cpf: String = "123.456.789-00"
+    private set
+
+    constructor()
+
+    fun pessoaInfo() = "$nome e $cpf"
+
 }
+
 fun main() {
     val denis = Pessoa()
-
-    println(denis.nome)
-    println(denis.cpf)
+    println(denis.pessoaInfo())
 }
